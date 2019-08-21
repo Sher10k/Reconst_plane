26817f76c5c7ed041f73dfa7e323ab39d1dedd5d
Roman <suhanovroman96@yandex.ru>
2019-08-21 19:38:31 +0300

    Start_recon_plane

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/calib3d.hpp>
+#include <opencv2/aruco/charuco.hpp>
+
+#include <Eigen/Eigen>
+
+#include <zcm/zcm.h>
+#include <zcm/zcm-cpp.hpp>
+
+using namespace std;
+using namespace cv;
+using namespace Eigen;
+using namespace zcm;
+
+
+int main() //int argc, char *argv[]
+{
+    
+    return 0;
+}
